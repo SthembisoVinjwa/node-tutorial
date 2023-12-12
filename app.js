cb8220383c41efe47eb7c99852932b092eb58abb
23745444 <23745444@sun.ac.za>
2023-12-12 18:27:43 +0200

    Update env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const env = require('dotenv').config()
 
 const app = express()
 
